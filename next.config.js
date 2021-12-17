47fd41e5c3a6aea4f63ddea1abe01a67d64e44e8
Brawer Nuñez <n.eedar@hotmail.com>
2021-12-17 14:02:13 -0500

    Primer commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    // reenderizar imagenes que no se encuentran dentro de mi app
+    domains: ['raw.githubusercontent.com']
+  }
 }
---
+++ pages/pokemones/[id].js
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Pokemon = ({ data}) => {
+  const router = useRouter();
+  console.log( router );
+
+  if ( router.isFallback ) {
+    return <p>Cargando... </p>
+  }
+  return (
+    <div>
+      <h1>{ data.name } número #{ data.id }</h1>
+      <Image src={ data.sprites.front_default } width={400} height={400}/>
+      <Link href="/">Volver al inicio</Link>
+    </div>
+  )
+}
+
+export default Pokemon;
+
+export const getStaticProps = async({ params }) => {
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ params.id }`);
+  const data = await response.json();
+  
+  return { props: { data }};
+}
+
+export const getStaticPaths = async() => {
+  // paths: arreglo que contiene a params (parámetros que recibira la url)
+  const paths = [
+    { params: { id: '1' } },
+    { params: { id: '2' } },
+  ]
+  return {
+    paths,
+    // fallback: true, false, 'blocking'
+    fallback: true,
+  }
+}
+/* 
+export const getServerSideProps = async({ params }) => {
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ params.id }`);
+  const data = await response.json();
+  
+  return { props: { data }};
+}
+ */
